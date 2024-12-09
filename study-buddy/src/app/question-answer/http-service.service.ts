ee645d7181c5d8b0f2ea38828e6824c51b901284
Pratyush Bindal <bindalpratyush2010@gmail.com>
2024-12-09 20:33:26 +0300

    Extra commit

--- study-buddy/src/app/question-answer/http-service.service.ts
+++ study-buddy/src/app/question-answer/http-service.service.ts
@@ -10,22 +10,6 @@ export class HttpSerivceService {
   private URL = 'http://localhost:8000';
   constructor(private http: HttpClient) { }
 
-//   getUsers() {
-
-//     return this.http.get(this.URL + '/get_users')
-//   }
-//   editUser(data: any) {
-
-//     return this.http.post(this.URL + '/update_full', data)
-//   }
-//   checkData(data: any) {
-
-//     return this.http.post(this.URL + '/check_data', data).pipe(map((res: any) => {
-
-//       return res
-//     }))
-//   }
-
 post_custom_prompt(data: any) {
 
     return this.http.post(this.URL + "/custom_prompt", data).pipe(map((res:any) => {
