84d601b23238df432766df9eb6ceda85531a40af
Pratyush Bindal <bindalpratyush2010@gmail.com>
2025-05-03 12:33:16 +0300

    /api

--- Backend/main.py
+++ Backend/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
 Token Validation is standardised accross the functions. If a token is valid, the function will return data as normal. If not, a False is returned by the token validator, resulting in a 401 error in the frontend
 '''
 
-@app.post("/custom_prompt")
+@app.post("/api/custom_prompt")
 def post_custom_prompt(prompt: PostCustomPromptModel, request: Request):
         
     token_res = validate_student(request.headers.get('token'))
@@ -33,7 +33,7 @@ def post_custom_prompt(prompt: PostCustomPromptModel, request: Request):
         return custom_prompt(prompt.customPrompt, db.getCurrentNotesByToken(request.headers.get('token')))
 
 
-@app.get("/summarise")
+@app.get("/api/summarise")
 def post_summarise(request: Request):
 
     token_res = validate_student(request.headers.get('token'))
@@ -42,7 +42,7 @@ def post_summarise(request: Request):
     else:
         return summariser(db.getCurrentNotesByToken(request.headers.get('token')))
 
-@app.get("/get_questions")
+@app.get("/api/get_questions")
 def get_questions(request: Request):
 
     token_res = validate_student(request.headers.get('token'))
@@ -51,7 +51,7 @@ def get_questions(request: Request):
     else:
         return make_questions(db.getCurrentNotesByToken(request.headers.get('token')))
 
-@app.post("/check_question")
+@app.post("/api/check_question")
 def post_check_questions(res: PostCheckAnswersModel, request: Request):
 
     token_res = validate_student(request.headers.get('token'))
@@ -60,7 +60,7 @@ def post_check_questions(res: PostCheckAnswersModel, request: Request):
     else:
         return check_question(res.question, res.answer, db.getCurrentNotesByToken(request.headers.get('token')))
 
-@app.get("/get_flashcards")
+@app.get("/api/get_flashcards")
 def get_flashcards(request: Request):
 
     token_res = validate_student(request.headers.get('token'))
@@ -70,17 +70,17 @@ def get_flashcards(request: Request):
         return flashcards(db.getCurrentNotesByToken(request.headers.get('token'))) 
 
 
-@app.post("/create_student")
+@app.post("/api/create_student")
 def create_user_post(user: PostStudentModel):
 
     return create_user(Student(name=user.name, email=user.email, password=user.password))
 
-@app.post("/check_student_login")
+@app.post("/api/check_student_login")
 def check_student_login_post(user: PostLoginCheckStudentModel):
 
     return check_student_login(user.email, user.password)
 
-@app.get("/get_student_credentials")
+@app.get("/api/get_student_credentials")
 def get_student_by_token(request: Request):
 
     token_res = validate_student(request.headers.get('token'))
@@ -90,7 +90,7 @@ def get_student_by_token(request: Request):
         name, email, id = token_res  # Unpack the validated data
         return {"name": name, "email": email, "id": id}
     
-@app.post("/edit_user")
+@app.post("/api/edit_user")
 def edit_user(newDetails: editUserModel, request: Request):
 
     token_res = validate_student(request.headers.get('token'))
@@ -101,7 +101,7 @@ def edit_user(newDetails: editUserModel, request: Request):
         return editUser(newDetails.newName, newDetails.email, newDetails.oldPassword, newDetails.newPassword)
     
 
-@app.post("/change_current_notes")
+@app.post("/api/change_current_notes")
 def change_current_notes(newNoteName: classes.PostChangeNotes, request: Request):
 
     token_res = validate_student(request.headers.get('token'))
@@ -113,7 +113,7 @@ def change_current_notes(newNoteName: classes.PostChangeNotes, request: Request)
         db.changeCurrentNotes(request.headers.get('token'), newNoteName.newNoteName)
 
 
-@app.post("/add_notes")
+@app.post("/api/add_notes")
 def post_add_notes(request: Request, sectionName: str = Form(...), file: UploadFile = File(...), ):
 
     token_res = validate_student(request.headers.get('token'))
@@ -149,7 +149,7 @@ def post_add_notes(request: Request, sectionName: str = Form(...), file: UploadF
             return {"message": "Incorrect filetype, must be PDF"}
         
 
-@app.post("/get_all_user_notes_tree")
+@app.post("/api/get_all_user_notes_tree")
 def get_user_notes_in_tree(request: Request):
 
     token_res = validate_student(request.headers.get('token'))
@@ -161,7 +161,7 @@ def get_user_notes_in_tree(request: Request):
         return getAllNotesTree(token_res[1])
     
 
-@app.post("/get_currently_selected_note")
+@app.post("/api/get_currently_selected_note")
 def getCurrentlySelectedNotesByToken(request: Request):
     
     token_res = validate_student(request.headers.get('token'))
