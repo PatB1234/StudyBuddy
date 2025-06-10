343cf0b8740b38cc86bdd8dbd52758c930ba09a1
Pratyush Bindal <bindalpratyush2010@gmail.com>
2025-06-10 21:29:18 +0300

    Added Functionality to custom-prompt page

--- study-buddy/src/app/custom-prompt/custom-prompt.component.ts
+++ study-buddy/src/app/custom-prompt/custom-prompt.component.ts
@@ -1,4 +1,4 @@
-import { Component, Renderer2 } from '@angular/core';
+import { Component, Renderer2, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -10,6 +10,8 @@ import {MatButtonModule} from '@angular/material/button';
 import { HttpClient } from '@angular/common/http';
 import {MatCardModule} from '@angular/material/card';
 import { AppComponent } from '../app.component';
+import { MarkdownModule } from 'ngx-markdown';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-custom-prompt',
@@ -23,7 +25,8 @@ import { AppComponent } from '../app.component';
 		MatInputModule,
 		ReactiveFormsModule,
 		MatButtonModule,
-		MatCardModule
+		MatCardModule,
+		MarkdownModule
   ],
   templateUrl: './custom-prompt.component.html',
   styleUrl: './custom-prompt.component.css'
@@ -35,7 +38,10 @@ export class CustomPromptComponent {
 	constructor(private http: HttpClient, private renderer: Renderer2) { }
 
 	URL: any = AppComponent.URL;
-
+	private _snackBar = inject(MatSnackBar);
+	openSnackBar(message: string, action: string) {
+		this._snackBar.open(message, action);
+	}	
   	//Custom Prompt Funcs
 	customPromptForm = new FormGroup({
 		customPrompt: new FormControl(''),
@@ -43,6 +49,7 @@ export class CustomPromptComponent {
 	result: any = '';
 
 	onSubmit() {
+		this._snackBar.open("Please wait while we process your request", "Dismiss")
 		this.http.post(this.URL + "/custom_prompt", this.customPromptForm.value).subscribe((res: any) => {
 			
 
