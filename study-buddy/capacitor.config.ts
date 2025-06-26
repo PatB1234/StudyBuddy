e3fec6c7bdf0d69c7aa7ca9e397f7fcf490436b8
Pratyush Bindal <bindalpratyush2010@gmail.com>
2025-06-26 15:30:01 +0300

    Started working on deploying to IOS

---
+++ study-buddy/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.studdybuddy.pratyush',
+  appName: 'StuddyBuddy',
+  webDir: 'dist/study-buddy/browser'
+};
+
+export default config;
